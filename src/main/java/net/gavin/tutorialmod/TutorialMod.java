ab5bfc497198f9d5f539f6c3d37cd6df7db480ac
StellyrGames <stellyrgames@gmail.com>
2024-05-22 16:56:37 -0400

    ruby info added

--- src/main/java/net/gavin/tutorialmod/TutorialMod.java
+++ src/main/java/net/gavin/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.gavin.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.gavin.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,5 +13,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
+		ModItems.registerModItems();
 	}
 }
