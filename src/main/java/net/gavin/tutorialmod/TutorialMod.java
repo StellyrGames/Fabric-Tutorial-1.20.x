a3f73fc4339a9a59b59dd9938cd617994a5278cc
StellyrGames <stellyrgames@gmail.com>
2024-06-04 18:05:20 -0400

    loot tables for blocks, tools added, tool levels added, crafting added, smelting added

--- src/main/java/net/gavin/tutorialmod/TutorialMod.java
+++ src/main/java/net/gavin/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.gavin.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.gavin.tutorialmod.block.ModBlocks;
+import net.gavin.tutorialmod.item.ModItemGroups;
 import net.gavin.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +14,9 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
 
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
